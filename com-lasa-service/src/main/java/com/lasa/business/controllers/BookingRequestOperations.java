252da6a79c819a84e160e26ffd86ae9fe74477cb
mynhung01 <nhungldmse151295@fpt.edu.vn>
2021-10-10 12:07:40 +0700

    add description swagger

--- com-lasa-service/src/main/java/com/lasa/business/controllers/BookingRequestOperations.java
+++ com-lasa-service/src/main/java/com/lasa/business/controllers/BookingRequestOperations.java
@@ -6,6 +6,7 @@
 package com.lasa.business.controllers;
 
 import com.lasa.data.entity.BookingRequest;
+import io.swagger.annotations.ApiParam;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,32 +23,41 @@ public interface BookingRequestOperations {
 
     @GetMapping
     ResponseEntity<?> getBookingRequests(
-            @RequestParam(value = "page", required = false, defaultValue = "0") Integer pageNumber,
-            @RequestParam(value = "size", required = false, defaultValue = "10") Integer pageSize,
-            @RequestParam(value = "studentId", required = false) Integer studentId,
-            @RequestParam(value = "slotId", required = false) Integer slotId
+            @ApiParam(name ="page",type ="integer",value = "Paging",defaultValue = "0") @RequestParam Integer pageNumber,
+            @ApiParam(name ="size",type ="integer",value ="The size you want view", defaultValue = "10") @RequestParam Integer pageSize,
+            @ApiParam(name ="studentId",type ="integer",value ="Get student by id") @RequestParam Integer studentId,
+            @ApiParam(name ="slotId",type ="integer",value ="Get slot by id", required = false) @RequestParam Integer slotId
             );
-
     @RequestMapping(value = "/status")
     ResponseEntity<?> getBookingRequestsByStatus(
-            @RequestParam(value = "page", required = false, defaultValue = "0") Integer pageNumber,
-            @RequestParam(value = "size", required = false, defaultValue = "10") Integer pageSize,
-            @RequestParam(value = "value", required = true) Integer status
+            @ApiParam(name ="page",type ="integer",value = "Paging",required = false, defaultValue = "0") @RequestParam Integer pageNumber,
+            @ApiParam(name ="size",type ="integer",value = "Size",required = false, defaultValue = "10") @RequestParam Integer pageSize,
+            @ApiParam(name ="status",type ="integer",value = "Status of booking request") @RequestParam  Integer status
     );
 
 
     @GetMapping(value = "/{id}")
-    public BookingRequest getBookingRequestById(@PathVariable("id") Integer id);
+    public BookingRequest getBookingRequestById(
+            @ApiParam(name = "id", type = "integer", value = "Get booking request by id")
+            @PathVariable("id") Integer id);
 
     @GetMapping(value = "/{id}/questions")
-    public BookingRequest getBookingRequestWithQuestionsById(@PathVariable("id") Integer id);
+    public BookingRequest getBookingRequestWithQuestionsById(
+            @ApiParam(name = "id", type = "integer", value = "Get question by id")
+            @PathVariable("id") Integer id);
     
     @PostMapping
-    public BookingRequest createBookingRequest(@RequestBody BookingRequest bookingRequest);
+    public BookingRequest createBookingRequest(
+            @ApiParam(name = "bookingRequest", type = "body", value = "Add a new booking request")
+            @RequestBody BookingRequest bookingRequest);
     
     @PutMapping
-    public BookingRequest updateBookingRequest(@RequestBody BookingRequest BookingRequest);
+    public BookingRequest updateBookingRequest(
+            @ApiParam(name = "bookingRequest", type = "body", value = "Update a booking request by id")
+            @RequestBody BookingRequest BookingRequest);
     
     @DeleteMapping
-    public void deleteBookingRequests(@RequestBody List<Integer> ids);
+    public void deleteBookingRequests(
+             @ApiParam(name = "ids", type = "body", value = "By id, you may remove booking request")
+            @RequestBody List<Integer> ids);
 }
--- com-lasa-service/src/main/java/com/lasa/business/controllers/StudentOperations.java
+++ com-lasa-service/src/main/java/com/lasa/business/controllers/StudentOperations.java
@@ -6,6 +6,7 @@
 package com.lasa.business.controllers;
 
 import com.lasa.data.entity.Student;
+import io.swagger.annotations.ApiParam;
 import java.util.List;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,14 +27,22 @@ public interface StudentOperations {
     public List<Student> findAll();
 
     @GetMapping(value = "/{id}")
-    public Student findByStudentId(@PathVariable Integer id);
+    public Student findByStudentId(
+            @ApiParam(name = "id", type = "integer", value = "By id, you may find a student", required = true)
+            @PathVariable Integer id);
 
     @PostMapping
-    public Student createStudent(@RequestBody Student student);
+    public Student createStudent(
+            @ApiParam(name = "student", type = "body", value = "Create a new student", required = true)
+            @RequestBody Student student);
 
     @PutMapping
-    public Student updateStudent(@RequestBody Student student);
+    public Student updateStudent(
+             @ApiParam(name = "student", type = "body", value = "Update a student", required = true)
+            @RequestBody Student student);
 
     @DeleteMapping
-    public void deleteStudents(@RequestBody List<Integer> ids);
+    public void deleteStudents(
+             @ApiParam(name = "ids", type = "body", value = "Remove a student by id", required = true)
+            @RequestBody List<Integer> ids);
 }
