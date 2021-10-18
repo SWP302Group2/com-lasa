ea879de6425fbcb9f3125f1edcfaa8a3d501fbac
hai anh <anhndhse151389@fpt.edu.vn>
2021-10-18 09:39:05 +0700

    add description example for page

--- com-lasa-data/src/main/java/com/lasa/data/entity/utils/page/BookingRequestPage.java
+++ com-lasa-data/src/main/java/com/lasa/data/entity/utils/page/BookingRequestPage.java
@@ -1,6 +1,7 @@
 package com.lasa.data.entity.utils.page;
 
 import com.lasa.data.entity.BookingRequest_;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,10 +11,30 @@ import org.springframework.data.domain.Sort;
 @Setter
 @NoArgsConstructor
 public class BookingRequestPage {
+
+    private static final String SORT_BY_ALLOW_VALUES =
+            BookingRequest_.ID + ","
+            + BookingRequest_.STUDENT_ID + ","
+            + BookingRequest_.TOPIC_ID + ","
+            + BookingRequest_.SLOT_ID + ","
+            + BookingRequest_.STATUS;
+
+    @ApiModelProperty(name = "page", value = "number of page", dataType = "Integer")
     private Integer page = 0;
+
+    @ApiModelProperty(name = "size", value = "number of element in page", dataType = "Integer")
     private Integer size = 10;
+
+    @ApiModelProperty(
+            name = "sortBy",
+            value = "select a field to sort",
+            dataType = "String",
+            allowableValues = SORT_BY_ALLOW_VALUES)
     private String sortBy = BookingRequest_.ID;
+
+    @ApiModelProperty(name = "orderBy", value = "select sort direction", dataType = "Direction")
     private Sort.Direction orderBy = Sort.Direction.ASC;
+    @ApiModelProperty(name = "paging", value = "select return a page or not", dataType = "boolean")
     private boolean paging = true;
 
 }
