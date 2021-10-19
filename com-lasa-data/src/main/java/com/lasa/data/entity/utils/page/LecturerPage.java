ab55aa3e1a5b028f92251eb57678c1efda79616b
mynhung01 <nhungldmse151295@fpt.edu.vn>
2021-10-19 13:53:25 +0700

    Add description for page

--- com-lasa-data/src/main/java/com/lasa/data/entity/utils/page/LecturerPage.java
+++ com-lasa-data/src/main/java/com/lasa/data/entity/utils/page/LecturerPage.java
@@ -1,6 +1,8 @@
 package com.lasa.data.entity.utils.page;
 
+
 import com.lasa.data.entity.Lecturer_;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,9 +12,27 @@ import org.springframework.data.domain.Sort;
 @Setter
 @NoArgsConstructor
 public class LecturerPage {
+    @ApiModelProperty(name = "page", value = "number of page", dataType = "Integer")
     private Integer page = 0;
+    
+    @ApiModelProperty(name = "size", value = "number of element in page", dataType = "Integer")
     private Integer size = 10;
+    
+    @ApiModelProperty(
+            name = "sortBy",
+            value = "select a field to sort",
+            dataType = "String",
+            allowableValues =
+                            Lecturer_.ID + "," +
+                            Lecturer_.NAME + "," +
+                            Lecturer_.EMAIL + "," +                           
+                            Lecturer_.STATUS + ","
+    )
     private String sortBy = Lecturer_.ID;
+     
+    @ApiModelProperty(name = "orderBy", value = "select sort direction", dataType = "Direction")
     private Sort.Direction orderBy = Sort.Direction.ASC;
+    
+    @ApiModelProperty(name = "paging", value = "select return a page or not", dataType = "boolean")
     private boolean paging = true;
 }
--- com-lasa-data/src/main/java/com/lasa/data/entity/utils/page/StudentPage.java
+++ com-lasa-data/src/main/java/com/lasa/data/entity/utils/page/StudentPage.java
@@ -2,6 +2,7 @@ package com.lasa.data.entity.utils.page;
 
 import com.lasa.data.entity.Slot_;
 import com.lasa.data.entity.Student_;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,9 +12,27 @@ import org.springframework.data.domain.Sort;
 @Setter
 @NoArgsConstructor
 public class StudentPage {
+    @ApiModelProperty(name = "page", value = "number of page", dataType = "Integer")
     private Integer page = 0;
+    
+    @ApiModelProperty(name = "size", value = "number of element in page", dataType = "Integer")
     private Integer size = 10;
+     @ApiModelProperty(
+            name = "sortBy",
+            value = "select a field to sort",
+            dataType = "String",
+            allowableValues =
+                            Student_.ID + "," +
+                            Student_.MAJOR_ID + "," +
+                            Student_.MSSV + "," +  
+                            Student_.EMAIL + "," + 
+                            Student_.STATUS + ","
+    )
     private String sortBy = Student_.ID;
+     
+    @ApiModelProperty(name = "orderBy", value = "select sort direction", dataType = "Direction")
     private Sort.Direction orderBy = Sort.Direction.ASC;
+     
+    @ApiModelProperty(name = "paging", value = "select return a page or not", dataType = "boolean")
     private boolean paging = true;
 }
