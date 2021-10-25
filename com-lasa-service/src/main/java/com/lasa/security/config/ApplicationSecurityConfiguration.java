90a57e6e5e0eb53d1ea974b9fadedfe71762796a
hai anh <anhndhse151389@fpt.edu.vn>
2021-10-25 16:22:04 +0700

    update allow uuid header

--- com-lasa-service/src/main/java/com/lasa/security/config/ApplicationSecurityConfiguration.java
+++ com-lasa-service/src/main/java/com/lasa/security/config/ApplicationSecurityConfiguration.java
@@ -103,7 +103,7 @@ public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapt
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
+        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token", "uuid"));
         configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
