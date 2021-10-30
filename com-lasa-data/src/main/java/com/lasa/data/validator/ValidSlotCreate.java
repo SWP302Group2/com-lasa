a71ca245d5dc0ba063c28c50b16d25cae31182b7
hai anh <anhndhse151389@fpt.edu.vn>
2021-10-30 20:25:58 +0700

    small update for annotation

--- com-lasa-data/src/main/java/com/lasa/data/validator/ValidSlotCreate.java
+++ com-lasa-data/src/main/java/com/lasa/data/validator/ValidSlotCreate.java
@@ -8,7 +8,7 @@ import javax.validation.Payload;
 import java.lang.annotation.*;
 
 @Documented
-@Target({ElementType.TYPE_USE})
+@Target({ElementType.TYPE})
 @Constraint(validatedBy = SlotCreateValidator.class)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ValidSlotCreate {
