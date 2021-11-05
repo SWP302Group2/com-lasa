7c448734ddd8ec891b78caa83393773f34605afb
hai anh <anhndhse151389@fpt.edu.vn>
2021-11-05 18:21:55 +0700

    fix time stamp

--- com-lasa-service/src/main/java/com/lasa/security/utils/model/ResponseObject.java
+++ com-lasa-service/src/main/java/com/lasa/security/utils/model/ResponseObject.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Getter;
 import springfox.documentation.annotations.ApiIgnore;
 
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 
@@ -12,17 +13,16 @@ import java.util.HashMap;
 @ApiIgnore
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class ResponseObject {
-    private String timestamp;
     private int status;
     private String error;
     private HashMap<String, String> errors;
     private String message;
     private String path;
-    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    private LocalDateTime timestamp;
 
     @Builder
-    public ResponseObject(String timestamp, int status, String error, HashMap<String, String> errors, String message, String path) {
-        this.timestamp = timestamp;
+    public ResponseObject( int status, String error, HashMap<String, String> errors, String message, String path) {
+        this.timestamp = LocalDateTime.now();
         this.status = status;
         this.error = error;
         this.errors = errors;
