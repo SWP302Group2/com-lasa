21a53f15963985f3058a3df399d48a0f56f23562
hai anh <anhndhse151389@fpt.edu.vn>
2021-11-15 10:55:44 +0700

    add request mapping for admin controller

--- com-lasa-service/src/main/java/com/lasa/business/controllers/implv1/AdminController.java
+++ com-lasa-service/src/main/java/com/lasa/business/controllers/implv1/AdminController.java
@@ -8,9 +8,13 @@ import com.lasa.data.model.request.AdminRequestModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
+@RequestMapping("api/v1/admins")
 public class AdminController implements AdminOperations {
 
     private final AdminService adminService;
